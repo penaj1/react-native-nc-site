b7bdc6b56c80cf8c6ad81fadf3e5f9561d953e83
Janice Pena <jipena94@gmail.com>
2022-04-13 18:12:55 -0700

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from "react-native";
 import { Card, ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 // import { FlatList } from "react-native-gesture-handler";
 
 const mapStateToProps = (state) => {
@@ -43,6 +44,26 @@ class About extends Component {
       );
     };
 
+    if (this.props.partners.isLoading) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
+    }
+    if (this.props.partners.errMess) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Text>{this.props.partners.errMess}</Text>
+          </Card>
+        </ScrollView>
+      );
+    }
     return (
       <ScrollView>
         <Mission />
