0649d04d883942d8556e3b6db40d67cf1a89ca2a
Janice Pena <jipena94@gmail.com>
2022-04-19 19:09:24 -0700

    Swipe and Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -185,3 +185,8 @@ export const addComment = (comment) => ({
   type: ActionTypes.ADD_COMMENT,
   payload: comment,
 });
+
+export const deleteFavorite = (campsiteId) => ({
+  type: ActionTypes.DELETE_FAVORITE,
+  payload: campsiteId,
+});
